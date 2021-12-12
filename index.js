f9321f0e7dea948f316c42395cc22f430b01f70e
John <john.hoe@gmail.com>
2021-12-13 03:39:34 +0800

    408 - Follow class and set up own dataseed.js

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
+// express
+
+// mongoose
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+const db_name = 'dev_yelpcamp'
+const _mongoose_opts = {
+//   autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(`mongodb://localhost:27017/${db_name}`)
+    .then(() => {
+    console.log(`${db_name} connected`)
+    })
+    .catch((err) => {
+    console.error(err)
+    })
+// mongoose
 
 // express
 app.use(express.static(path.join(__dirname, 'static')));
@@ -17,11 +38,13 @@ const _port = 8888;
 app.listen(_port, () => {
     console.log(`yYelpCamp, listening on ${_port}`);
 })
+// express
 
-const { _feIndex, _fe404 } = require('./routes/frontend/app');
+const { _feIndex, _feTESTAddCamp, _fe404 } = require('./routes/frontend/app');
 
 // frontend
 app.get('/', _feIndex);
+app.get('/addcampground', _feTESTAddCamp);
 
 // 404s
 app.get('*', _fe404);
