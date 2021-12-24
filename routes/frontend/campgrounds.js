496a88a5200d437be7e01d9c4256d95e8e06849e
John <john.hoe@gmail.com>
2021-12-25 00:45:58 +0800

    492 - flash errors, discovered a bug on whether campground exists, corrected the checks. whew!

--- routes/frontend/campgrounds.js
+++ routes/frontend/campgrounds.js
@@ -27,6 +27,11 @@ const _feListCampgrounds = catchAsync(async (req, res) => {
 const _feShowCampground = catchAsync(async (req, res) => {
     const { id } = req.params;
     const campGround = await Campground.findById(id).populate('reviews');
+    if (campGround.isDelete) {
+        const msg = 'Cannot find that campground.';
+        req.flash('error', msg)
+        return res.redirect('/campgrounds');
+    }
     res.render('campground.ejs', { campGround });
 })
 
@@ -57,17 +62,27 @@ const _deleteCampground = catchAsync(async (req, res) => {
 
 const _feEditCampground = catchAsync(async (req, res) => {
     const { id } = req.params;
+    const campGround = await Campground.findById(id);
     switch (req.method) {
         case "PUT":
             const editCampground = req.body.campground;
-            // console.log(editCampground[title]);
-            await Campground.findByIdAndUpdate(id, editCampground, { new: true, runValidators: true });
-            const msg = 'Camp edited successfully!';
-            req.flash('success', msg);
-            res.redirect(`/campgrounds/${id}`)
+            if (campGround.isDelete) {
+                const msg = 'Cannot find that campground.';
+                req.flash('error', msg)
+                res.redirect('/campgrounds');
+            } else {
+                await Campground.findByIdAndUpdate(id, editCampground, { new: true, runValidators: true });
+                const msg = 'Camp edited successfully!';
+                req.flash('success', msg);
+                res.redirect(`/campgrounds/${id}`)
+            }
             break;
         default:
-            const campGround = await Campground.findById(id);
+            if (campGround.isDelete) {
+                const msg = 'Cannot find that campground.';
+                req.flash('error', msg)
+                return res.redirect('/campgrounds');
+            }
             res.render('editCampground.ejs', { campGround });
     }
 })
