2041d7a3fde6c8270eaae90afbabb94d562cf94d
John <john.hoe@gmail.com>
2021-12-26 22:14:09 +0800

    510-511 - register and login routes

--- routes/user/user.js
+++ routes/user/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/expressError');
 const User = require('../../models/user');
+const passport = require('passport');
 
 // joi
 const userSchema = require('../../joiSchemas/joi_user');
@@ -21,16 +22,38 @@ const validateUser = (req, res, next) => {
 const _feRegister = catchAsync( async (req, res) => {
     switch (req.method) {
         case "POST":
-            // console.log(req.body.user);
-            const { email, username, password } = req.body.user;
-            res.send(`${email} ${username} ${password}`);
-            break;
+            try {
+                const { email, username, password } = req.body.user;
+                const newUser = new User({ email, username });
+                const registeredUser = await User.register(newUser, password);
+                req.flash('success', `Welcome to yYelpCamp, ${registeredUser.username}!`);
+                res.redirect('/campgrounds');
+                break;
+            } catch (err) {
+                req.flash('error', err.message);
+                res.redirect('/register');
+                break;
+            }
         default:
             res.render('register.ejs');
     }
 })
 
+const _feLogin = catchAsync(async (req, res) => {
+    switch (req.method) {
+        case "POST":
+            const { username } = req.body;
+            req.flash('success', `Welcome back, ${username}!`);
+            res.redirect('/campgrounds');
+            break;
+        default:
+            res.render('login.ejs')
+    }
+})
+
 router.get('/register', _feRegister);
 router.post('/register', validateUser, _feRegister);
+router.get('/login', _feLogin);
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), _feLogin);
 
 module.exports = router;
