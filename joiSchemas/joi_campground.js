4a294c69698af0fd5777b91645bb2ffcdce2e475
John <john.hoe@gmail.com>
2021-12-30 03:11:31 +0800

    escapeHTML - 2

--- joiSchemas/joi_campground.js
+++ joiSchemas/joi_campground.js
@@ -6,7 +6,7 @@ const campgroundSchema = Joi.object({
   campground: Joi.object({
     // title: Joi.string().required().escapeHTML(),
     title: Joi.string().required(),
-    location: Joi.string().required().escapeHTML(),
+    location: Joi.string().required(),
     // image: Joi.string().required(),
     price: Joi.number().required().min(1),
     description: Joi.string().required().escapeHTML(),
