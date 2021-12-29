f5aea5bdcc5a5b08f2bf73ab422cabb85deea504
John <john.hoe@gmail.com>
2021-12-29 21:18:38 +0800

    556-562 - styles, frontpage, map controls etc.

--- static/js/campgroundMap.js
+++ static/js/campgroundMap.js
@@ -1,17 +1,14 @@
 mapboxgl.accessToken = MAPBOX_TOKEN;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: mapbox_coordinates, // starting position [lng, lat]
-    zoom: 12 // starting zoom
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: mapbox_coordinates, // starting position [lng, lat]
+  zoom: 12, // starting zoom
 });
 
-const marker = new mapboxgl.Marker({ color: 'black' })
-    .setLngLat(mapbox_coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(
-            `${camptitle}`
-        )
-    )
-    .addTo(map);
+const marker = new mapboxgl.Marker({ color: "black" })
+  .setLngLat(mapbox_coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`${camptitle}`))
+  .addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl());
